99c61c79fb28c13b0623803185e97c2b7aa4536e
Stepan Koci <stepan.koci@imatic.cz>
2013-03-28 17:03:29 +0100

    Test project

---
+++ Fixtures/TestProject/TestKernel.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Imatic\Bundle\TestingBundle\Fixtures\TestProject;
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+class TestKernel extends Kernel
+{
+    private $config;
+
+    public function __construct()
+    {
+        parent::__construct('test', true);
+        $config = $this->getRootDir() . '/config/config.yml';
+
+        if (!file_exists($config)) {
+            throw new \RuntimeException(sprintf('The config file "%s" does not exist.', $config));
+        }
+
+        $this->config = $config;
+    }
+
+    public function registerBundles()
+    {
+        $bundles = array(
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
+            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new \Sonata\AdminBundle\SonataAdminBundle(),
+            new \Sonata\BlockBundle\SonataBlockBundle(),
+            new \Sonata\CacheBundle\SonataCacheBundle(),
+            new \Sonata\jQueryBundle\SonatajQueryBundle(),
+            new \Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new \Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle()
+        );
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->config);
+    }
+
+    public function serialize()
+    {
+        return serialize(array($this->config));
+    }
+
+    public function unserialize($str)
+    {
+        call_user_func_array(array($this, '__construct'), unserialize($str));
+    }
+
+    public static function getClass()
+    {
+        return get_called_class();
+    }
+}
